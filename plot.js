917351e415cb4dd6f5a406e6b9a5460e70531510
ahsuehsueh <hsueh1536@gmail.com>
2016-11-16 18:14:45 +0800

    Add files via upload

---
+++ plot.js
@@ -0,0 +1,19 @@
+function plot()
+{
+var width = 1024;
+var height = 1700;
+var ctrl = d3.select("body").append("svg").attr("width", width).attr("height", height);
+d3.csv("https://ahsuehsueh.github.io/d3/userPost.csv", 
+	function(data)
+	{
+		var ln = data.length;
+		console.log(data);
+		var maxy = d3.max(data, function(d){ return d.like; }); 
+		var lines = d3.line().x(function(d,i){return i*(width/ln);}).y(function(d){return height-d.like});  
+		ctrl.append("path").data([data]).attr("d", lines).attr("stroke", "red").attr("fill", "none");
+		
+	}
+
+);
+
+}
